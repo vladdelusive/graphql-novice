a62800d067588b976c9d116a2e58f558a2ecb3bb
vladdelusive <mr.tovstochub@mail.ru>
2021-04-14 14:21:57 +0300

    add: client to backend

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { RootComponent } from './components/root';
+
+function App() {
+  return (
+    <div className="App">
+      <RootComponent />
+    </div>
+  );
+}
+
+export default App;
